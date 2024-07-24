5f320a2239b6635010646780b7a0effc1dc75c96
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2024-07-24 20:18:55 +0300

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BreezeForecast());
+}
+
+class BreezeForecast extends StatelessWidget {
+  const BreezeForecast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
